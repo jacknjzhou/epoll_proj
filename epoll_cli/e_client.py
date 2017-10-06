12da2ef4cb6224f7729f1612ca146bcc1896372f
jerryzhow <jerryzhou@digiwin.biz>
2017-10-06 15:32:00 +0800

    增加配置文件设置,增加传入目标存储路径,增加设置目标存储路径异常情况,改存储为/tmp目录之下

--- epoll_cli/e_client.py
+++ epoll_cli/e_client.py
@@ -4,41 +4,92 @@ import struct
 import socket
 import os
 import sys
+import getopt
 """
 @function:采用tcp/epoll方式进行文件上传,并可以指定文件上传路径,指定目标存储路径.
 @此处采用struct方式进行序列化编码,然后再svr端进行解码处理
 
 @次序为 filename,filesize,file_destpath
 """
+#add local info file
+import confs
+
+conf_obj = confs.Config('server.ini')
+
+ip = conf_obj.get("SERVER","server.ip","127.0.0.1")
+port = conf_obj.get("SERVER","server.port",18888)
+if not isinstance(port,int):
+  port = int(port)
+
+print "ip:",ip," port:",port
+
 
 INFO_STRUCT = '128s1I128s'
 
 #创建客户端socket对象
 clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #服务端IP地址和端口号元组
-server_address = ('127.0.0.1',18888)
+server_address = (ip,port)
 #客户端连接指定的IP地址和端口号
 
 #filename = "protobuf-2.5.0.tar.gz"
 FILEINFO_SIZE = struct.calcsize(INFO_STRUCT)
-BUFSIZE =1024
+BUFSIZE =1024*4
+
+#采用支持的命令行方法进行输入参数(采用命令参数的方式进行输入上传的文件+路径)
+r_argv = sys.argv[1:]
+
+if not r_argv:
+    print "输入的参数为空."
+    sys.exit(1)
+try:
+    opts, args = getopt.getopt(r_argv,"hu:p:",["uploadfile","destpath"])
+    print "***"*10
+    print opts
+    print "***"*10
+    print args
+except getopt.GetoptError as e:
+    print e
+    print traceback.format_exc()
+    print "Error: script.py -u <uploadfile> -p <destpath>"
+    print "Error: script.py --uploadfile <uploadfile> --destpath <destpaht>"
+    sys.exit(2)
+
+filename = None
+dest_path = None
+
+for opt,arg in opts:
+    if opt == '-h':
+        print "Error: script.py -u <uploadfile> -p <destpaht>"
+        print "Error: script.py --uploadfile <uploadfile> --destpath <destpaht>"
+        sys.exit(1)
+    if opt in ('-u','--uploadfile'):
+        filename = arg
+
+    if opt in ('-p','--destpath'):
+        dest_path = arg
+
+print "filename:",filename
+print "destpath:",dest_path
 
 #输入数据
-filename = raw_input('please input upload filename:')
+# filename = raw_input('please input upload filename:')
 
 if os.path.exists(filename):
     pass
 else:
     print "input filename [%s] not exist" %(filename,)
-    if clientsocket:
-        clientsocket.close()
     sys.exit(1)
-    
-dest_path ="/tmp"
+
+if dest_path is None:
+    dest_path ="/tmp"
+
+base_filename = os.path.basename(filename)
+print "basename:",base_filename
 #retry connection
 clientsocket.connect(server_address)
 
-fhead = struct.pack(INFO_STRUCT,filename,os.stat(filename).st_size,dest_path)
+fhead = struct.pack(INFO_STRUCT,base_filename,os.stat(filename).st_size,dest_path)
 clientsocket.send(fhead)
 #print type(fhead)
 
